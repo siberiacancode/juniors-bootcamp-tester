3f1fbfb88b17a259ccab379c1bc1a089b15d9a68
Awsjhbc <exibp@mail.ru>
2026-04-01 22:14:29 +0700

    feature/home-page 🧊 husky init

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,7 @@
 import { eslint } from '@siberiacancode/eslint';
 
-export default eslint(
-  {
-    typescript: true,
-    react: true,
-    jsx: true
-  },
-  {
-    rules: {
-      'perfectionist/sort-interfaces': 'off'
-    }
-  }
-);
+export default eslint({
+  typescript: true,
+  react: true,
+  jsx: true
+});
